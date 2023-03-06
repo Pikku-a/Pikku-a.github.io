34c392ebbfcbd8c3f939676ce6556fa2bf3c4c7d
pikku-a <aapo1mattila@gmail.com>
2023-03-06 17:02:55 +0200

    jaa...

--- scripts/language-handling.js
+++ scripts/language-handling.js
@@ -2,18 +2,17 @@
 //© Pikku-a 2023
 function languageChange(lang /*, id1, id2, ... */) {
 	$(document).ready(function() {
-		console.log("Version 0.4");
+		console.log("Version 0.5");
 		$.getJSON("https://pikkua.com/text/pikkuacom.json",function(data) {
 			for (var i = 0; i < arguments.length+1; i++) {
-				var t = arguments[i+1];
 				if (lang == "en") {
-					$(`#${t}`).text(data.general.en[arguments[i+1]]);
+					$(`#${arguments[i+1]}`).text(data.general.en[arguments[i+1]]); //`#${arguments[i+1]}` <- Ainakaan tämä osa ei toimi
 					localStorage.setItem("language","en");
 				}else if (lang == "fi") {
-					$(`#${t}`).text(data.general.fi[arguments[i+1]]);
+					$(`#${arguments[i+1]}`).text(data.general.fi[arguments[i+1]]);
 					localStorage.setItem("language","fi");
 				}else if (lang == "es") {
-					$(`#${t}`).text(data.general.es[arguments[i+1]]);
+					$(`#${arguments[i+1]}`).text(data.general.es[arguments[i+1]]);
 					localStorage.setItem("language","es");
 				}
 				console.log("Language changed and saved.");
