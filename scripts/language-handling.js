844defde4a62270e445f2580c1b21194927de450
pikku-a <aapo1mattila@gmail.com>
2023-03-06 15:58:45 +0200

    testailua yhä

--- scripts/language-handling.js
+++ scripts/language-handling.js
@@ -5,13 +5,13 @@ function languageChange(lang /*, id1, id2, ... */) {
 		$.getJSON("https://pikkua.com/text/pikkuacom.json",function(data) {
 			for (var i = 0; i < arguments.length+1; i++) {
 				if (lang == "en") {
-					$("#${arguments[i+1]}").text(data.general.en.arguments[i+1]);
+					$(`#${arguments[i+1]}`).text(data.general.en.arguments[i+1]);
 					localStorage.setItem("language","en");
 				}else if (lang == "fi") {
-					$("#${arguments[i+1]}").text(data.general.fi.arguments[i+1]);
+					$(`#${arguments[i+1]}`).text(data.general.fi.arguments[i+1]);
 					localStorage.setItem("language","fi");
 				}else if (lang == "es") {
-					$("#${arguments[i+1]}").text(data.general.es.arguments[i+1]);
+					$(`#${arguments[i+1]}`).text(data.general.es.arguments[i+1]);
 					localStorage.setItem("language","es");
 				}
 				console.log("Language changed and saved.");
