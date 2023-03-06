39c864db582676b2770c1bec54c38c7265306b0c
pikku-a <aapo1mattila@gmail.com>
2023-03-06 15:54:03 +0200

    kieliskriptin testausta

---
+++ scripts/language-handling.js
@@ -0,0 +1,21 @@
+///Language handling script
+
+function languageChange(lang /*, id1, id2, ... */) {
+	$(document).ready(function() {
+		$.getJSON("https://pikkua.com/text/pikkuacom.json",function(data) {
+			for (var i = 0; i < arguments.length+1; i++) {
+				if (lang == "en") {
+					$("#${arguments[i+1]}").text(data.general.en.arguments[i+1]);
+					localStorage.setItem("language","en");
+				}else if (lang == "fi") {
+					$("#${arguments[i+1]}").text(data.general.fi.arguments[i+1]);
+					localStorage.setItem("language","fi");
+				}else if (lang == "es") {
+					$("#${arguments[i+1]}").text(data.general.es.arguments[i+1]);
+					localStorage.setItem("language","es");
+				}
+				console.log("Language changed and saved.");
+			}
+		})
+	});
+}
