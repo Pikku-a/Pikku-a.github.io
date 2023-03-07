ba061b7eca3d5c175355465dfc2f0df607e54294
pikku-a <aapo1mattila@gmail.com>
2023-03-07 15:37:59 +0200

    -

--- scripts/language-handling.js
+++ scripts/language-handling.js
@@ -6,20 +6,28 @@ function languageChange(lang /*, id1, id2, ... */) {
 		$.getJSON("https://pikkua.com/text/pikkuacom.json",function(data) {
 			for (var i = 0; i < arguments.length-1; i++) {
 				console.log(i);
-				console.log(arguments[i+1]);
-				let str = arguments[i+1].toString().substring(1); //Removes #
-				if (lang == "en") {
-					$(`${arguments[i+1]}`).text(data.general.en[str]); //data.general.en[arguments[i+1]]
-					localStorage.setItem("language","en");
-				}else if (lang == "fi") {
-					$(`${arguments[i+1]}`).text(data.general.fi[str]);
-					localStorage.setItem("language","fi");
-				}else if (lang == "es") {
-					$(`${arguments[i+1]}`).text(data.general.es[str]);
-					localStorage.setItem("language","es");
+				console.log(arguments[i]);
+				console.log(lang);
+				//let str = arguments[i].toString().substring(1); //Removes #?
+				//console.log(str);
+				if (i!=0) { //Don't do this with the first argument
+					if (lang == "en") {
+						console.log("Language is en");
+						$(`${arguments[i]}`).text(data.general.en[arguments[i]]); //data.general.en[arguments[i]]
+						localStorage.setItem("language","en");
+					}else if (lang == "fi") {
+						console.log("Language is fi");
+						$(`${arguments[i]}`).text(data.general.fi[arguments[i]]);
+						localStorage.setItem("language","fi");
+					}else if (lang == "es") {
+						console.log("Language is es");
+						$(`${arguments[i]}`).text(data.general.es[arguments[i]]);
+						localStorage.setItem("language","es");
+					}
+					console.log("Loaded string "+String(i));
 				}
-				console.log("Language changed and saved.");
 			}
+			console.log("Language loaded or changed and saved.");
 		})
 	});
 }
