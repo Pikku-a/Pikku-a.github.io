16cbc55793ffcb6093f9afe63c74de278d048f37
pikku-a <aapo1mattila@gmail.com>
2023-03-07 16:28:38 +0200

    -

--- scripts/language-handling.js
+++ scripts/language-handling.js
@@ -1,28 +1,27 @@
 //Language handling script
 //© Pikku-a 2023
-function languageChange(lang /*, id1, id2, ... */) {
+function languageChange(lang, ...args) { //Getting the argument works if it's added here, but otherwise it won't work
 	$(document).ready(function() {
-		console.log("Version 0.91");
+		console.log("Version 0.9.2");
 		$.getJSON("https://pikkua.com/text/pikkuacom.json",function(data) {
-			for (var i = 0; i < arguments.length-1; i++) {
+			for (var i = 0; i < args.length; i++) {
 				console.log(i);
-				console.log(arguments[i]);
-				console.log(lang);
-				//let str = arguments[i].toString().substring(1); //Removes #?
+				console.log(String(args[i])); //String(args[i]) taitaa olla sama kuin `${args[i]}`
+				console.log("Lanuage: "+lang);
+				//let str = args[i].toString().substring(1); //Removes #?
 				//console.log(str);
 				if (i!=0) { //Don't do this with the first argument
 					if (lang == "en") {
-						console.log("Language is en");
-						$(`${arguments[i]}`).text(data.general.en[arguments[i]]); //data.general.en[arguments[i]]
+						$(`${args[i]}`).text(data.general.en[args[i]]); //data.general.en[args[i]]
 						localStorage.setItem("language","en");
 					}else if (lang == "fi") {
-						console.log("Language is fi");
-						$(`${arguments[i]}`).text(data.general.fi[arguments[i]]);
+						$(`${args[i]}`).text(data.general.fi[args[i]]);
 						localStorage.setItem("language","fi");
 					}else if (lang == "es") {
-						console.log("Language is es");
-						$(`${arguments[i]}`).text(data.general.es[arguments[i]]);
+						$(`${args[i]}`).text(data.general.es[args[i]]);
 						localStorage.setItem("language","es");
+					}else{
+						console.log("Language not selected, so using English.");
 					}
 					console.log("Loaded string "+String(i));
 				}
