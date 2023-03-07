3ff87b7ece8a5bc9c8386317342e610c225792b2
pikku-a <aapo1mattila@gmail.com>
2023-03-07 17:05:56 +0200

    -

--- scripts/language-handling.js
+++ scripts/language-handling.js
@@ -1,8 +1,8 @@
 //Language handling script
 //© Pikku-a 2023
-function languageChange(lang, ...args) { //Getting the argument works if it's added here, but otherwise it won't work
+function languageChange(lang, ...args) {
 	$(document).ready(function() {
-		console.log("Version 0.9.4");
+		console.log("Version 0.9.5");
 		$.getJSON("https://pikkua.com/text/pikkuacom.json",function(data) {
 			for (var i = 0; i < args.length; i++) {
 				console.log(i);
@@ -12,13 +12,13 @@ function languageChange(lang, ...args) { //Getting the argument works if it's ad
 				console.log("Lanuage: "+lang);
 				if (i!=0) { //Don't do this with the first argument
 					if (lang == "en") {
-						$(args[i]).text(data.general.en.str); //data.general.en[args[i]] || data.general.en[str]
+						$(args[i]).text(data.general.en[str]); //data.general.en[args[i]] || data.general.en.str
 						localStorage.setItem("language","en");
 					}else if (lang == "fi") {
-						$(args[i]).text(data.general.fi.str);
+						$(args[i]).text(data.general.fi[str]);
 						localStorage.setItem("language","fi");
 					}else if (lang == "es") {
-						$(args[i]).text(data.general.es.str);
+						$(args[i]).text(data.general.es[str]);
 						localStorage.setItem("language","es");
 					}else{
 						console.log("Language not selected, so using English.");
